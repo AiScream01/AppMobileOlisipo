bf04a9acfc7c7dbc0207762eac9b8938cc57ecf7
Ana Figueiredo <beatriz1002figueiredo@gmail.com>
2024-09-07 19:47:34 +0100

    Update recuperar_password_email_screen.dart

--- lib/presentation/recuperar_password_email_screen/recuperar_password_email_screen.dart
+++ lib/presentation/recuperar_password_email_screen/recuperar_password_email_screen.dart
@@ -3,62 +3,47 @@ import 'package:rui_pedro_s_application11/core/app_export.dart';
 import 'package:rui_pedro_s_application11/widgets/custom_elevated_button.dart';
 import 'package:rui_pedro_s_application11/widgets/custom_text_form_field.dart';
 
-// ignore: must_be_immutable
 class RecuperarPasswordEmailScreen extends StatelessWidget {
-  RecuperarPasswordEmailScreen({Key? key})
-      : super(
-          key: key,
-        );
+  RecuperarPasswordEmailScreen({Key? key}) : super(key: key);
 
-  TextEditingController emailController = TextEditingController();
+  final emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        extendBody: true,
-        extendBodyBehindAppBar: true,
-        resizeToAvoidBottomInset: false,
-        body: Container(
-          width: SizeUtils.width,
-          height: SizeUtils.height,
-          decoration: BoxDecoration(
-            color: theme.colorScheme.onPrimaryContainer,
-            boxShadow: [
-              BoxShadow(
-                color: appTheme.black900.withOpacity(0.3),
-                spreadRadius: 2.h,
-                blurRadius: 2.h,
-                offset: Offset(
-                  10,
-                  10,
-                ),
-              ),
-            ],
-            image: DecorationImage(
-              image: AssetImage(
-                ImageConstant.imgLogin,
-              ),
-              fit: BoxFit.cover,
-            ),
-          ),
+        resizeToAvoidBottomInset: true,
+        body: SingleChildScrollView(
           child: Container(
-            width: double.maxFinite,
-            padding: EdgeInsets.symmetric(
-              horizontal: 21.h,
-              vertical: 30.v,
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              color: theme.colorScheme.onPrimaryContainer,
+              image: DecorationImage(
+                image: AssetImage(ImageConstant.imgLogin),
+                fit: BoxFit.cover,
+              ),
             ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CustomImageView(
-                  imagePath: ImageConstant.imgOlisipoLogoblack,
-                  height: 118.v,
-                  width: 246.h,
+            child: Center(
+              // Para centralizar o conteúdo
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 27),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 48),
+                    CustomImageView(
+                      imagePath: ImageConstant.imgOlisipoLogoblack,
+                      height: 118,
+                      width: 270,
+                      fit: BoxFit.contain,
+                    ),
+                    SizedBox(height: 33),
+                    _buildPasswordRecoveryForm(context),
+                  ],
                 ),
-                SizedBox(height: 40.v),
-                _buildPasswordRecovery(context),
-              ],
+              ),
             ),
           ),
         ),
@@ -66,63 +51,60 @@ class RecuperarPasswordEmailScreen extends StatelessWidget {
     );
   }
 
-  /// Section Widget
-  Widget _buildPasswordRecovery(BuildContext context) {
+  Widget _buildPasswordRecoveryForm(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(
-        horizontal: 20.h,
-        vertical: 30.v,
-      ),
-      decoration: AppDecoration.outlineGray.copyWith(
-        borderRadius: BorderRadiusStyle.roundedBorder35,
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 17),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(35),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
       ),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          SizedBox(
-            width: 200.h,
+          // Centralização do título
+          Center(
             child: Text(
               "Recuperar Password",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
               style: theme.textTheme.displaySmall,
+              textAlign: TextAlign.center,
             ),
           ),
-          SizedBox(height: 40.v),
+          SizedBox(height: 25),
           Align(
             alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.only(left: 5.h),
-              child: Text(
-                "Email",
-                style: theme.textTheme.bodyMedium,
-              ),
-            ),
+            child: Text("Email", style: theme.textTheme.bodyMedium),
           ),
-          SizedBox(height: 10.v),
+          SizedBox(height: 2),
           CustomTextFormField(
             controller: emailController,
             textInputAction: TextInputAction.done,
           ),
-          SizedBox(height: 30.v),
+          SizedBox(height: 30),
           CustomElevatedButton(
-            width: 155.h,
+            height: 60,
+            width: 155,
             text: "Recuperar",
+            onPressed: () {
+              // Lógica para recuperar a password
+            },
           ),
-          SizedBox(height: 25.v),
+          SizedBox(height: 19),
           GestureDetector(
             onTap: () {
               Navigator.pushNamed(context, AppRoutes.loginScreen);
             },
-            child: SizedBox(
-              width: 150.h,
-              child: Text(
-                "Já tem conta?\nFaz o login",
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                style: CustomTextStyles.titleMediumNunitoPrimary,
+            child: Text(
+              "Já tem conta? Faz o login",
+              textAlign: TextAlign.center,
+              style: theme.textTheme.bodyMedium?.copyWith(
+                color: theme.colorScheme.primary,
               ),
             ),
           ),
